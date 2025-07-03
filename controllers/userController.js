68115c7824908c1e61b294c5ba1233c91febef73
Zendacross97 <sidhchakraboty66@gmail.com>
2025-07-03 15:20:14 +0530

    logIn backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const UserServices = require('../services/userServices');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
 exports.signUp = async (req, res) => {  
     try {
@@ -22,5 +24,40 @@ exports.signUp = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({error: error.message});
+    }
+};
+
+exports.getLoginPage = (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/logIn.html'));
+};
+
+function token(id) {
+    return jwt.sign({ UserId: id }, 'secret_key')
+}
+
+exports.logIn = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Login credentials are incomplete' });
+        }
+        const userDetails = await UserServices.getUserByEmail(email);
+        if (userDetails.length === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        bcrypt.compare(password, userDetails[0].password, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: 'Something went wrong' });
+            }
+            else if (!result) {// result is false if password does not match
+                return res.status(401).json({ error: 'User not authorized' });
+            }
+            else {
+                const userToken = token(userDetails[0].id);
+                res.status(200).json({ message: 'Login successful', token: userToken });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signUp', userController.signUp);
+router.get('/login', userController.getLoginPage);
+router.post('/login', userController.logIn);
 
 module.exports = router;
